602be65675626fbd6482eb211b4ed0dba53dcde6
atsunori.nishiyama <nishiyama1980@icloud.com>
2019-08-30 21:24:37 +0000

    新規登録機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      render.new
+    end
   end
 
   def edit
@@ -25,6 +36,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest, :image)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
 end
