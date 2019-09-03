aed64d089e4f2ed34fce94844b688c5e77c0e4a2
atsunori.nishiyama <nishiyama1980@icloud.com>
2019-09-03 21:21:29 +0000

    relationship追加、follow,unfollowアクション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,24 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   has_many :dishes
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationships, source: :user
+  
+  def follow(oter_user)
+    unless self == oter_user
+      self.relationships.find_or_create_by(follow_id: oter_user.id)
+    end
+  end
+  
+  def unfollow(oter_user)
+    relationship = self.relationships.find_by(follow_id: oter_user.id)
+    relationship.destroy if relationship
+  end
+  
+  def following?(oter_user)
+    self.followings.include?(oter_user)
+  end
+  
 end
